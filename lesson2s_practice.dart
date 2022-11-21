3ef128f2b5bc9c18d27635b222ca8555036d95ad
aigeram <118278771+aigeram@users.noreply.github.com>
2022-11-21 23:53:37 +0600

    lesson2s_practice.dart

--- lesson2s_practice.dart
+++ lesson2s_practice.dart
@@ -1,1 +1,22 @@
+ void main() {
+  String students ="Fk-3";
+  int num =15;
+  double grade =75.9;
+  bool isChildren =false;
 
+  Map array ={"students":"Fk-3",num:"15",grade:"75.9"};
+
+  dynamic d = 'double';
+  d=75.9;
+  d=true;
+
+  var s =3;
+
+  final g =75.9;
+
+  const n =15;
+
+  print(15);
+
+print(String);
+}
